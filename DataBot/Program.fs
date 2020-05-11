a293cdf9f5c04e0bc097cc0e5bf5cec439d6188b
Victor Hogemann <victor.hogemann@gmail.com>
2020-05-11 18:40:14 +0100

    Generating images

--- DataBot/Program.fs
+++ DataBot/Program.fs
@@ -2,11 +2,31 @@
 open System
 open System.Windows.Forms.DataVisualization.Charting
 
-[<STAThread>]
 [<EntryPoint>]
 let main argv =
 
+    let data = Data.Brazil.GetGdp()
+
+    let chart = new Chart();
+    chart.Palette <- ChartColorPalette.EarthTones
     
+    let series = chart.Series.Add "TEST"
+    
+    series.ChartType <- SeriesChartType.Spline
+    
+    for (year, value) in data do
+        series.Points.AddXY(string year, value) |> ignore
+    
+    let ca = new ChartArea()
+    ca.AxisX <- new Axis()
+    ca.AxisY <- new Axis()
+
+    ca.BorderDashStyle <- ChartDashStyle.Solid
+
+    chart.ChartAreas.Add ca
+
+    chart.SaveImage("test.png", Drawing.Imaging.ImageFormat.Png)
+
 
 
     0
