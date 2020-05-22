748785c42ee900a0b38d36aeb9f5e8d8d86d8156
v0h004v <victor@hogemann.com>
2020-05-22 18:34:25 +0100

    4 images per tweet

--- DataBot/Twitter.fs
+++ DataBot/Twitter.fs
@@ -19,6 +19,10 @@ let twitterAccessTokenSecret =
 let lockFilePath = 
     "FS_DATA_BOT_LAST_REPLY" |> getEnv
 
+type Reply =
+| Tweet of PublishTweetOptionalParameters
+| Error of string
+
 type FeedReader() =
     do 
         Auth.SetUserCredentials(
@@ -29,56 +33,63 @@ type FeedReader() =
         TweetinviConfig.CurrentThreadSettings.TweetMode <- TweetMode.Extended
     let botUser = User.GetAuthenticatedUser().ScreenName
 
-    let rec postReply (userHandle:string) (tweetId:int64) (replies:MemoryStream list) = async {
-        match replies with
-        | [] -> return ()
-        | stream :: tail ->
-            let media = ResizeArray [ Upload.UploadBinary(stream.ToArray()) ]
-            let options = PublishTweetOptionalParameters()
-            options.Medias <- media
-            options.InReplyToTweetId <- Nullable(tweetId)
-            let! response = TweetAsync.PublishTweet(sprintf "@%s" userHandle , options) |>Async.AwaitTask
-            return! postReply botUser (response.Id) tail
+    let execute (commands:Command.Command list) = async {
+        let! images =
+            commands
+            |> Seq.map ( fun command -> async {
+                let line = Graph.Line command
+                let upload = Upload.UploadBinary( line.ToArray() )
+                return upload
+            })
+            |> Async.Parallel
+        return PublishTweetOptionalParameters(Medias = ResizeArray(images))
     }
-
-    let reply (mentions: IMention seq) = 
+        
+    let reply (mentions: IMention seq) = async { 
+        let! replies =
             mentions
             |> Seq.filter (fun mention -> not (isNull mention.Text))
-            |> Seq.map (fun mention ->
-                printfn "replying to: %s" mention.Text
-                let commands = mention.Text.ToLowerInvariant() |> Command.Parse
-                try
-                    let replies = 
-                        seq { 
-                            for command in commands do
-                                yield Graph.Line command
-                        } |> Seq.toList
-                    postReply mention.CreatedBy.ScreenName mention.Id replies
-                with
-                | exn -> 
-                    printfn "Error posting reply %A" exn
-                    Async.Sleep 0
+            |> Seq.map ( fun mention ->
+                let commands =
+                    mention.Text.ToLowerInvariant()
+                    |> Command.Parse
+                    |> List.chunkBySize 4
+                commands, mention.Id, mention.CreatedBy.ScreenName
+            )
+            |> Seq.map (fun (commands, mentionId, userHandle) ->
+                    commands
+                    |> List.map ( fun command -> async {
+                        let! reply = command |> execute
+                        reply.InReplyToTweetId <- Nullable(mentionId)
+                        return TweetAsync.PublishTweet(sprintf "@%s" userHandle, reply)
+                    })
+                    |> Async.Parallel
             )
             |> Async.Parallel
-            |> Async.Ignore
-
+        return replies |> Seq.collect id
+    }
+                
     let start () = async {
         printfn "fetching tweets"
         let mayBeLastMention = GetLatestMention() |> Seq.tryHead
-        let mentions = 
+        let maybeMentions = 
             match mayBeLastMention with
             | Some mention -> 
                 let parameters = MentionsTimelineParameters()
                 parameters.SinceId <- mention.Id
                 parameters.MaximumNumberOfTweetsToRetrieve <- 100
-                Timeline.GetMentionsTimeline(parameters)
-            | None -> Timeline.GetMentionsTimeline()
-        if not(isNull mentions) then do!
-            mentions
-            |> Seq.map (fun mention -> { Id = mention.Id; TimeStamp = mention.CreatedAt })
-            |> SaveMentions
-            |> ignore
-            reply mentions
+                Timeline.GetMentionsTimeline(parameters) |> Option.ofObj
+            | None -> Timeline.GetMentionsTimeline() |> Option.ofObj
+        
+        match maybeMentions with
+        | Some mentions ->
+            do! mentions
+                |> Seq.map (fun mention -> { Id = mention.Id; TimeStamp = mention.CreatedAt })
+                |> SaveMentions
+                |> Async.Ignore
+            return! reply mentions |> Async.Ignore
+        | None ->
+            return ()
     }
 
     member __.Start () = async {
