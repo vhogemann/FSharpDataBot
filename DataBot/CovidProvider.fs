093c95ea24bfe0a9a122de4e9d7d67d8e62ef321
v0h004v <victor@hogemann.com>
2020-05-29 12:11:09 +0100

    New Covid graphs. Testing in production! Yay!

--- DataBot/CovidProvider.fs
+++ DataBot/CovidProvider.fs
@@ -4,18 +4,34 @@ open ZedGraph
 
 type Covid = JsonProvider<"covidsample.json", SampleIsList=true>
 
+let totalVsNew (total: (double*double) seq) (newCases: (double*double) list) =
+    let totalValues =
+        total
+        |> Seq.map( fun (_,v) -> v )
+   
+    let newValues =
+        newCases
+        |> Seq.map( fun (_,v) -> v )
+    Seq.zip totalValues newValues
 
 let folder (state: (double*double) list) (current:double*double): (double*double) list =
-    List.empty
+    let (_, prev) = state |> List.last    
+    let (dt, curr) = current
+    let diff = System.Math.Abs( prev - curr )
+    [(dt, diff)] @ state
 
 type CovidIndicator (code:string, name:string, values:Covid.Root[]) =
     member __.Name = name
     member __.Code = code
     member __.Deaths = values |> Seq.map (fun value -> (double (value.Date.DateTime |> XDate), double value.Deaths))
-    member this.NewDeaths =
-        this.Deaths
+    member this.NewDeaths = this.Deaths |> Seq.toList |> List.fold folder [(0.0, 0.0)]
+    member this.TotalVsNewDeaths = totalVsNew (this.Deaths) (this.NewDeaths)
     member __.Confirmed = values |> Seq.map (fun value -> (double (value.Date.DateTime |> XDate), double value.Confirmed))
+    member this.NewConfirmed = this.Confirmed |> Seq.toList |> List.fold folder [(0.0, 0.0)]
+    member this.TotalVsNewConfirmed = totalVsNew (this.Confirmed) (this.NewConfirmed)
     member __.Recovered = values |> Seq.map (fun value -> (double (value.Date.DateTime |> XDate), double value.Recovered))
+    member this.NewRecovered = this.Recovered |> Seq.toList |> List.fold folder [(0.0, 0.0)]
+    member this.TotalVsNewRecovered = totalVsNew (this.Recovered) (this.NewRecovered)
 
 let AsyncFetch code name=
     let baseUrl = sprintf "https://api.covid19api.com/total/dayone/country/%s" code
--- DataBot/Twitter.fs
+++ DataBot/Twitter.fs
@@ -66,11 +66,15 @@ type FeedReader() =
                     )
                     |> Async.Parallel
             let deaths = indicators |> Seq.map (fun country -> country.Name, country.Deaths)
+            let deathsNxT = indicators |> Seq.map (fun country -> country.Name, country.TotalVsNewDeaths)
             let confirmed = indicators |> Seq.map (fun country -> country.Name, country.Confirmed)
+            let confirmedNxT = indicators |> Seq.map (fun country -> country.Name, country.TotalVsNewConfirmed)
             let recovered = indicators |> Seq.map (fun country -> country.Name, country.Recovered)
             return seq {
                 yield Graph.Line "Covid-19 - Deaths" deaths None None true true
+                yield Graph.Line "Covid-10 - New vs Total Deaths" deathsNxT None None true true
                 yield Graph.Line "Covid-19 - Confirmed" confirmed None None true true
+                yield Graph.Line "Covid-19 - New vs Total Confirmed" confirmedNxT None None true true
                 yield Graph.Line "Covid-19 - Recovered" recovered None None true true
             }
     }
