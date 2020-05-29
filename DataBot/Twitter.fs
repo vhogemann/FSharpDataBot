33e950619d82c5c48c9707bbd78ef96036311d1e
v0h004v <victor@hogemann.com>
2020-05-29 12:29:12 +0100

    Commenting out for now

--- DataBot/Twitter.fs
+++ DataBot/Twitter.fs
@@ -72,9 +72,9 @@ type FeedReader() =
             let recovered = indicators |> Seq.map (fun country -> country.Name, country.Recovered)
             return seq {
                 yield Graph.Line "Covid-19 - Deaths" deaths None None true false true 
-                yield Graph.Line "Covid-10 - New vs Total Deaths" deathsNxT None None false true true
+                //yield Graph.Line "Covid-10 - New vs Total Deaths" deathsNxT None None false true true
                 yield Graph.Line "Covid-19 - Confirmed" confirmed None None true false true
-                yield Graph.Line "Covid-19 - New vs Total Confirmed" confirmedNxT None None false true true
+                //yield Graph.Line "Covid-19 - New vs Total Confirmed" confirmedNxT None None false true true
                 yield Graph.Line "Covid-19 - Recovered" recovered None None true false true
             }
     }
